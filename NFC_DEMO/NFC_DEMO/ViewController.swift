3d41aac168e646f0875ec0625e0ae8f35592b155
ankit bharti <ankit.bharti@adeptpros.com>
2018-05-03 18:04:45 +0530

    implemented to open and read the File

--- NFC_DEMO/NFC_DEMO/ViewController.swift
+++ NFC_DEMO/NFC_DEMO/ViewController.swift
@@ -23,5 +23,56 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func openDocument(_ sender: Any) {
+        
+        let documentPicker = UIDocumentPickerViewController(documentTypes:
+            ["com.apple.iwork.pages.pages",
+             "com.apple.iwork.numbers.numbers",
+             "com.apple.iwork.keynote.key",
+             "public.image",
+             "com.apple.application",
+             "public.item","public.data",
+             "public.content",
+             "public.audiovisual-content",
+             "public.movie",
+             "public.audiovisual-content",
+             "public.video",
+             "public.audio",
+             "public.text",
+             "public.data",
+             "public.zip-archive",
+             "com.pkware.zip-archive",
+             "public.composite-content",
+             "public.text"], in: .open)
+        documentPicker.delegate = self
+        present(documentPicker, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UIDocumentPickerDelegate,UINavigationControllerDelegate {
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let url = urls.first else {
+            print("couldn't pick document")
+            return
+        }
+        DispatchQueue.main.async {
+            self.messageLabel.text = "\(url)"
+        }
+        let documentInteractionController = UIDocumentInteractionController(url: url)
+        documentInteractionController.delegate = self
+        documentInteractionController.presentPreview(animated: true)
+    }
+    
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UIDocumentInteractionControllerDelegate {
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
 }
 
